1db0f93f1e93a760e212b3c7361ac0420afd8652
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2025-01-17 08:45:26 +0530

    add about us page component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+export default function Notice() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h2>About Us Page</h2>
+        </div>
+    )
+}
